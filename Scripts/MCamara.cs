45806d858af3795cb602750e5d94f45dbe4cb891
Moon Antonio <pinchodev91@gmail.com>
2017-03-28 18:56:03 +0200

    Add MCamara.cs

---
+++ Scripts/MCamara.cs
@@ -0,0 +1,26 @@
+﻿//                                  ┌∩┐(◣_◢)┌∩┐
+//																				\\
+// MCamara.cs (28/03/2017)														\\
+// Autor: Antonio Mateo (Moon Antonio) 									        \\
+// Descripcion:		Controlador de la camara									\\
+// Fecha Mod:		28/03/2017													\\
+// Ultima Mod:		Version inicial												\\
+//******************************************************************************\\
+
+#region Librerias
+using UnityEngine;
+#endregion
+
+namespace Pendulum.Controller
+{
+	/// <summary>
+	/// <para>Controlador de la camara</para>
+	/// </summary>
+	[AddComponentMenu("Pendulum/Controller/MCamara")]
+	public class MCamara : MonoBehaviour 
+	{
+		#region Variables Privadas
+
+		#endregion
+	}
+}
